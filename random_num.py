36e1bf02481507155b81f976538b1a650868cce7
pinky <basu.sanjana1619@gmail.com>
2021-04-30 00:41:46 -0500

    Adding random_num.py file

---
+++ random_num.py
@@ -0,0 +1,6 @@
+#To generate random number in Python, randint() function is used. This function is defined in random module.
+#Syntax:Syntax randint(a,b) ->range [a,b], meaning a <= N <= b
+
+import random
+random_num = random.randint(20,100)
+print(random_num)
